07b75d21de1b3d83af87db0d86d49895de81741e
Xixi <flyskyxi@gmail.com>
2015-09-07 21:13:09 -0400

    initial + voice button set up

---
+++ vioce magic/vioce magicTests/vioce_magicTests.swift
@@ -0,0 +1,36 @@
+//
+//  vioce_magicTests.swift
+//  vioce magicTests
+//
+//  Created by Sissi on 9/7/15.
+//  Copyright (c) 2015 Sissi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class vioce_magicTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
